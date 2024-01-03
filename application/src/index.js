b67b479cd2948be7019994b0a9e21b11ae39713e
Grokeen <ygreen0516@gmail.com>
2024-01-03 22:28:19 +0900

    clear

--- application/src/index.js
+++ application/src/index.js
@@ -1,15 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 
+// 2401031844 뭐지? 왜 앞에 대문자를 사용하니까 돼????
+import ItemList from "./shop/itemList";
+import BarsketPage from "./shop/barsketPage";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+
+// index.js -> ❌ App.js -> ./shop/
 // index.js가 하는 일은 App.js의 내용을 -> public/index.html에 박아넣는 것.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // 2401031703 경로 설정을 위해 BrowserRouter를 감싸주었다.
+  <BrowserRouter>
+    {/* 헤더 설정 import 해서, 태그로 만들 수 있다. Switch는 각각 다르게 설정 가능하다. */}
+    <Routes>
+      <Route path="/" element={<ItemList />} />
+      <Route path="barsketPage" element={<BarsketPage />} />
+    </Routes>
+
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ application/src/shop/barsketPage.js
@@ -0,0 +1,192 @@
+
+
+import "../App.css";
+import React, { useState, useEffect } from 'react';
+
+
+
+function App() {
+    // 세션 배열처리, 하드코딩,,,
+    const retrieveSessionItem = (key) => {
+        const item = sessionStorage.getItem(key);
+        return item ? JSON.parse(item) : null;
+    };
+
+    const imFile1inBasket = retrieveSessionItem('1adinBasket');
+    const imFile2inBasket = retrieveSessionItem('2adinBasket');
+    const imFile3inBasket = retrieveSessionItem('3adinBasket');
+    const imFile4inBasket = retrieveSessionItem('4adinBasket');
+    const imFile5inBasket = retrieveSessionItem('5adinBasket');
+    const imFile6inBasket = retrieveSessionItem('6adinBasket');
+    const imFile7inBasket = retrieveSessionItem('7adinBasket');
+    const imFile8inBasket = retrieveSessionItem('8adinBasket');
+
+    const arrayVal = [
+        imFile1inBasket,
+        imFile2inBasket,
+        imFile3inBasket,
+        imFile4inBasket,
+        imFile5inBasket,
+        imFile6inBasket,
+        imFile7inBasket,
+        imFile8inBasket
+    ];
+
+
+
+    console.log(arrayVal);
+
+
+
+    // 2138 문자형을 인트형으로 바꿔주는 애
+
+    let sumVal = 0;
+
+    const [sum, setSumVal] = useState(0);
+    useEffect(() => {
+        let count = 0;
+        
+        arrayVal.forEach((item) => {
+            try {
+                console.log(arrayVal[count].price + '');
+                const priceStrings = arrayVal[count].price;
+                const numericString=priceStrings.replace(/\D/g, '');
+                const number = parseInt(numericString, 10);
+
+                sumVal += number;
+                console.log(sumVal);
+            } catch (error) {
+            }
+            count++;
+            
+        });
+        setSumVal(sumVal);
+    },[sum]);
+    let deliPrice=3000;
+    // if(sum=>30000){
+    //     deliPrice=0;
+    // }
+    let totalPrice = deliPrice+sum;
+
+    // 이미지 파일
+    function importAll(r) {
+        let images = {};
+        r.keys().forEach((item) => { images[item.replace('./', '')] = r(item); });
+        return images;
+    }
+    const imagesJPGALL = importAll(require.context('../pic', false, /\.jpg$/));
+
+
+
+    return (
+        <div className="wrap">
+            <div className="container">
+                <div className="contents">
+                    {/* cartArea 이거 없는데? */}
+                    <div className="cartArea">
+                        <div className="cartBoard">
+                            <table className="tblForm">
+
+                                <colgroup>
+                                    {/* 2401030301 style 방식이 다르네 */}
+
+                                    <col style={{ width: '25px' }} />
+                                    <col style={{ width: '10%' }} />
+                                    <col style={{ width: '11%' }} />
+                                    <col style={{ width: '10%' }} />
+
+                                </colgroup>
+                                <thead>
+                                    <tr>
+                                        <th scope="col">상품명</th>
+                                        <th scope="col">구매가</th>
+                                        <th scope="col">수량</th>
+                                        <th scope="col">금액</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+
+                                    {/* -------------------------------------------------------------------- */}
+                                    {/* 2401032021 렌더링 반복문 */}
+                                    {arrayVal.map((item, index) => {
+                                        // Check if the item is not null
+                                        if (item) {
+                                            return (
+                                                <tr key={item.Id || index}> {/* Moved key to <tr> */}
+                                                    <td className="itmTarget">
+
+                                                        <div className="thum">
+                                                            <img src={imagesJPGALL[item.Id + '.jpg']} alt="" />
+                                                        </div>
+                                                        <div className="cont">
+                                                            {/* 상품명 */}
+                                                            <strong className="tit">{item.name}</strong>
+                                                        </div>
+
+
+                                                    </td>
+
+
+                                                    <td className="tdPrice">{/* 구매가 */}
+                                                        <span className="cost1" title="판매가" ><b>{item.price}</b>원</span></td>
+
+
+                                                    <td >{/* 수량 */}
+                                                        <span className="frmAmount">
+                                                            <button type="button" className="minus"
+                                                                onmousedown="">수량 감소</button>
+                                                            <input type="text" title="수량 직접입력" name="qty"
+                                                                data-sell-mon="9450"
+                                                                data-delivery-type="Y" data-each-delivery="N" value="1" data-delivery-amt="3000"
+                                                                data-delivery-free="30000" data-delivery-gubun="O" className="numeric" data-jirisansoo-yn="N"
+                                                                maxlength="5" />
+                                                            {/* <input type="text" title="수량 직접입력" name="qty"
+                                                                onChange="" data-sell-mon="9450"
+                                                                data-delivery-type="Y" data-each-delivery="N" value="1" data-delivery-amt="3000"
+                                                                data-delivery-free="30000" data-delivery-gubun="O" className="numeric" data-jirisansoo-yn="N"
+                                                                maxlength="5" /> */}
+                                                            <button type="button" className="plus"
+                                                                onmousedown="">수량 증가</button>
+                                                        </span></td>
+                                                    <td className="tdPrice">
+                                                        {/* 금액 */}
+                                                        <span className="cost1" title="금액"><b>{item.price}</b>원</span>
+                                                    </td>
+                                                </tr>
+                                            );
+                                        }
+                                        return null; // It's a good practice to return null for non-rendering cases
+                                    })}
+
+
+
+
+                                    {/* -------------------------------------------------------------------- */}
+                                </tbody>
+
+
+                                <tfoot>
+                                    <tr>
+                                        <td colSpan="8" className="tdTotal">
+                                            <span className="txt">총 금액</span>
+                                            <span className="cost"><b>{sum}</b>원</span>
+                                            
+
+                                            <span className="symbol plus">+</span>
+                                            <span className="txt">배송비</span>
+                                            <span className="cost"><b>{deliPrice}</b>원</span>
+                                            <span className="txt2">(3만원이상 구매 시 무료배송)</span>
+                                            <span className="symbol equal">=</span>
+                                            <span className="txt">결제 금액</span>
+                                            <span className="cost"><b>{totalPrice}</b>원</span>
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div></div></div></div></div>
+    );
+
+
+}
+export default App;
